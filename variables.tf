bc947a2bb028d7241ab6a678b4ecf15b1ad45282
Balraj Pasula <157344097+balrajpasula@users.noreply.github.com>
2025-08-07 16:28:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "ami_id" {
   description = "The AMI ID for the EC2 instance."
   type        = string
   
-  default     = "ami-08c3e8787b407b483"
+  default     = "ami-0f918f7e67a3323f0"
 }
 
 variable "instance_type" {
